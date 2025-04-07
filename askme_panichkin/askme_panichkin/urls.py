41b1b62c3d7bd28d66bfee79f2e58dd49108192a
Vladpin48 <vladpanich48@icloud.com>
2025-04-07 21:44:50 +0300

    Update urls.py

--- askme_panichkin/askme_panichkin/urls.py
+++ askme_panichkin/askme_panichkin/urls.py
@@ -21,12 +21,5 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
-    path('', views.index, name='index'),
-    path('hot', views.hot, name='hot'),
-    path('question/<int:question_id>', views.question, name='question'),
-    path('tag/<str:tag_name>/', views.tag, name='tag'),
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('ask/', views.ask, name='ask')
+    path('', include('app.urls'))
 ]
