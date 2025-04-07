5954962cdcf15a9b724efa705dfd0fff28fdfb75
Vladpin48 <vladpanich48@icloud.com>
2025-04-07 21:44:20 +0300

    Update urls.py

--- askme_panichkin/app/urls.py
+++ askme_panichkin/app/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('hot', views.hot, name='hot'),
     path('question/<int:question_id>', views.question, name='question'),
